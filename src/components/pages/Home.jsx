4f0441682ea50b8426a52873e36056465cd15b08
Trictonicmp <code_shadow@hotmail.com>
2022-06-22 01:12:18 -0500

    Add loading text

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,16 @@
-const Home = () => (
-  <section>
-    <h1>Home</h1>
-  </section>
-);
+import { useSelector } from 'react-redux';
+
+const Home = () => {
+  const state = useSelector((state) => state.appStatus);
+
+  return (
+    <section>
+      <h1>Home</h1>
+      {
+        state === 'LOADING' ? <p>loading</p> : <p>loaded</p>
+      }
+    </section>
+  );
+};
 
 export default Home;
