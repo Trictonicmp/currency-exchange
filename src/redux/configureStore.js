8adad2c3e735826757035eb60e8ad9560a96aab9
Trictonicmp <code_shadow@hotmail.com>
2022-06-22 01:04:34 -0500

    Add base currency reducer to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import currencies from './currencies/currencies';
 import appStatus from './app/appStatus';
+import baseCurrency from './currencies/baseCurrency';
 
 const store = configureStore({
   reducer: {
+    baseCurrency,
     currencies,
     appStatus,
   },
