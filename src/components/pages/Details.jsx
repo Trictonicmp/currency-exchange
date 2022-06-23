492e5d5261e9e1a009138dc335e4f3dabb0ba8bb
Trictonicmp <code_shadow@hotmail.com>
2022-06-23 11:02:04 -0500

    Add more details to details header

--- src/components/pages/Details.jsx
+++ src/components/pages/Details.jsx
@@ -1,11 +1,20 @@
 import { useSelector } from 'react-redux';
+import DetailsHeader from '../DetailsHeader';
+import styles from '../../css/components/details.module.css';
 
 const Details = () => {
   const country = useSelector((state) => state.selectedCountry);
-  console.log(country);
+  const baseCurrency = useSelector((state) => state.baseCurrency);
   return (
     <section>
-      <DetailsHeader country={country.name} flag={country.flag} />
+      <div className={styles.header}>
+        <DetailsHeader
+          name={country.name}
+          flag={country.flag}
+          todaysExchange={country.todaysExchange}
+          baseCurrency={baseCurrency}
+        />
+      </div>
       <h1>Details</h1>
     </section>
   );
